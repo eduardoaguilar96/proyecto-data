4e02aba63da9a4e718163c239cb16dfe23c0e390
eduardoaguilar96 <eduaguilar96@gmail.com>
2022-07-05 19:02:30 +0200

    algunos cambios

--- src/App.js
+++ src/App.js
@@ -1,38 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { Component } from 'react';
-import { FileUpload } from 'primereact/fileupload';
-import 'primeicons/primeicons.css';
-import 'primereact/resources/themes/lara-light-indigo/theme.css';
-import 'primereact/resources/primereact.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import React, { Component, useState } from "react";
+import { FileUpload } from "primereact/fileupload";
+import "primeicons/primeicons.css";
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.css";
 
 
 function App() {
- 
-const chooseOptions = {label: 'Cargar imagen', icon: 'pi pi-fw pi-plus'};
-const uploadOptions = {label: 'Uplaod', icon: 'pi pi-upload', className: 'p-button-success'};
-const cancelOptions = {label: 'Cancel', icon: 'pi pi-times', className: 'p-button-danger'};
+  const [prediction, setPrediction] = useState("");
+  const myUploader = (event) => {
+    
+    const formData = new FormData();
+    formData.append('image', event.files[0])
+    //event.files == files to upload
+  
+    fetch('http://dynucleus.com:8444/api/v1/predict', {
+      method: "POST",
+      body: formData,
 
-const myUploader = (event) => {
-  //event.files == files to upload
-console.log(event)
-}
- 
+    }).then(response => response.json())
+      .then(json => setPrediction(json.prediction))
+  }
+
   return (
     <div className="App">
-    <div className='contenedor'>
-      <h1>Lenguaje de signos</h1>
-      
-<FileUpload name="demo[]" url="./upload" chooseOptions={chooseOptions} uploadOptions={uploadOptions} cancelOptions={cancelOptions}  uploadHandler={myUploader}/>
- 
-      <div className='resultado'>
-        <h1>Resultado</h1>
+      <div className="contenedor">
+        <h1>Lenguaje de signos</h1>
+        <FileUpload
+          name="demo[]"
+          url="./upload"
+          customUpload
+          uploadHandler={myUploader}
+        />
+        <div className="resultado">
+          <h1>Resultado</h1>
+          {prediction}
+        </div>
       </div>
     </div>
-    </div>
   );
-
 }
 
 export default App;
